a79573e3440b9d767a27aa8d1054cb4ee06e374f
Brent O'Connor <epicserve@gmail.com>
2020-02-28 07:58:26 -0600

    Bump version to 3.1.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup
 
 setup(
     name='django-cache-url',
-    version='3.1.1',
+    version='3.1.2',
     url='https://github.com/epicserve/django-cache-url',
     license='MIT',
     author="Brent O'Connor",
