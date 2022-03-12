ffac5d4192570eddad1ae41982d2fdc258543984
Brent O'Connor <epicserve@gmail.com>
2022-03-12 16:11:50 -0600

    Fix tests

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,4 +1,8 @@
+from django import VERSION
+
 import django_cache_url
+
+redis_backend = 'django_redis.cache.RedisCache' if VERSION[0] < 4 else 'django.core.cache.backends.redis.RedisCache'
 
 
 #
@@ -9,7 +13,7 @@ def test_hiredis():
     url = 'hiredis://127.0.0.1:6379/0?key_prefix=site1'
     config = django_cache_url.parse(url)
 
-    assert config['BACKEND'] == 'django_redis.cache.RedisCache'
+    assert config['BACKEND'] == redis_backend
     assert config['LOCATION'] == 'redis://127.0.0.1:6379/0'
     assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
 
@@ -18,7 +22,7 @@ def test_hiredis_socket():
     url = 'hiredis:///path/to/socket/1?key_prefix=site1'
     config = django_cache_url.parse(url)
 
-    assert config['BACKEND'] == 'django_redis.cache.RedisCache'
+    assert config['BACKEND'] == redis_backend
     assert config['LOCATION'] == 'unix:/path/to/socket?db=1'
     assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
 
@@ -31,7 +35,7 @@ def test_redis():
     url = 'redis://127.0.0.1:6379/0?key_prefix=site1'
     config = django_cache_url.parse(url)
 
-    assert config['BACKEND'] == 'django_redis.cache.RedisCache'
+    assert config['BACKEND'] == redis_backend
     assert config['LOCATION'] == 'redis://127.0.0.1:6379/0'
 
 
@@ -39,7 +43,7 @@ def test_redis_socket():
     url = 'redis:///path/to/socket/1?key_prefix=site1'
     config = django_cache_url.parse(url)
 
-    assert config['BACKEND'] == 'django_redis.cache.RedisCache'
+    assert config['BACKEND'] == redis_backend
     assert config['LOCATION'] == 'unix:/path/to/socket?db=1'
     assert 'OPTIONS' not in config
 
@@ -48,6 +52,6 @@ def test_redis_with_password():
     url = 'redis://:redispass@127.0.0.1:6379/0'
     config = django_cache_url.parse(url)
 
-    assert config['BACKEND'] == 'django_redis.cache.RedisCache'
+    assert config['BACKEND'] == redis_backend
     assert config['LOCATION'] == 'redis://127.0.0.1:6379/0'
     assert config['OPTIONS']['PASSWORD'] == 'redispass'
