8d61eeef70be7fa52a2146cf98fd6a86505b47fa
Brent O'Connor <epicserve@gmail.com>
2022-12-10 15:34:30 -0600

    Move sleep time

--- scripts/create_release.py
+++ scripts/create_release.py
@@ -28,6 +28,8 @@ def update_version(version):
 def commit_and_tag(version):
     os.popen('git add setup.py')
     os.popen(f'git commit -m "Bump version to {version}"')
+    # without sleep it tags the previous commit
+    time.sleep(2)
     os.popen(f'git tag -a -m "" v{version}')
 
 
@@ -55,9 +57,6 @@ if __name__ == "__main__":
         new_version = input(f'What version would you like for this release (current version: {current_version})? ')
 
     update_version(new_version)
-
-    # without sleep it tags the previous commit
-    time.sleep(2)
 
     commit_and_tag(new_version)
 
