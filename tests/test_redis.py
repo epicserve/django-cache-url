02f8ab5501e2a003e4f4a9c98c95a1607063d4c9
Nagico <yjr888@vip.qq.com>
2022-12-27 11:49:16 +0800

    Simplify test cases of different Django versions

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,80 +1,62 @@
-import mock
-import importlib
 
-import django_cache_url
-
-#
-# HIREDIS
-#
-
-
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_hiredis_dj3():
-    importlib.reload(django_cache_url)
+# region HIREDIS url
+def test_hiredis_dj3(django_cache_url_dj3):
     url = 'hiredis://127.0.0.1:6379/0?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj3.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DEFAULT_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj3.DEFAULT_REDIS_BACKEND
     assert config['LOCATION'] == 'redis://127.0.0.1:6379/0'
     assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
 
 
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_hiredis_dj4():
-    importlib.reload(django_cache_url)
+def test_hiredis_dj4(django_cache_url_dj4):
     url = 'hiredis://127.0.0.1:6379/0?key_prefix=site1&pool_class=redis.BlockingConnectionPool'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj4.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DEFAULT_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj4.DEFAULT_REDIS_BACKEND
     assert config['LOCATION'] == 'redis://127.0.0.1:6379/0'
     assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
     assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
     assert config['OPTIONS']['pool_class'] == 'redis.BlockingConnectionPool'
+# endregion
 
 
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_hiredis_socket_dj3():
-    importlib.reload(django_cache_url)
+# region HIREDIS socket
+def test_hiredis_socket_dj3(django_cache_url_dj3):
     url = 'hiredis:///path/to/socket/1?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj3.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DEFAULT_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj3.DEFAULT_REDIS_BACKEND
     assert config['LOCATION'] == 'unix:///path/to/socket?db=1'
     assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
 
 
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_hiredis_socket_dj4():
-    importlib.reload(django_cache_url)
+def test_hiredis_socket_dj4(django_cache_url_dj4):
     url = 'hiredis:///path/to/socket/1?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj4.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DEFAULT_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj4.DEFAULT_REDIS_BACKEND
     assert config['LOCATION'] == 'unix:///path/to/socket?db=1'
     assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
     assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
 
 
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_hiredis_socket_with_username_password_dj3():
-    importlib.reload(django_cache_url)
+def test_hiredis_socket_with_username_password_dj3(django_cache_url_dj3):
     url = 'hiredis://foo:bar@/path/to/socket/1?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj3.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DJANGO_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj3.DJANGO_REDIS_BACKEND
     assert config['LOCATION'] == 'unix://foo:bar@/path/to/socket?db=1'
     assert 'PASSWORD' not in config.get('OPTIONS', {})
     assert config['KEY_PREFIX'] == 'site1'
     assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
 
 
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_hiredis_socket_with_username_password_dj4():
-    importlib.reload(django_cache_url)
+def test_hiredis_socket_with_username_password_dj4(django_cache_url_dj4):
     url = 'hiredis://foo:bar@/path/to/socket/1?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj4.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.BUILTIN_DJANGO_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
     assert config['LOCATION'] == 'unix://foo@/path/to/socket?db=1&password=bar'
     assert 'PASSWORD' not in config.get('OPTIONS', {})
     assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
@@ -82,216 +64,199 @@ def test_hiredis_socket_with_username_password_dj4():
     assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
 
 
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_hiredis_with_password_dj3():
-    importlib.reload(django_cache_url)
+def test_hiredis_with_password_dj3(django_cache_url_dj3):
     url = 'hiredis://:redispass@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj3.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DEFAULT_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj3.DEFAULT_REDIS_BACKEND
     assert config['LOCATION'] == 'redis://127.0.0.1:6379/0'
     assert config['OPTIONS']['PASSWORD'] == 'redispass'
     assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
 
 
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_hiredis_with_password_dj4():
-    importlib.reload(django_cache_url)
+def test_hiredis_with_password_dj4(django_cache_url_dj4):
     url = 'hiredis://:redispass@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj4.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.BUILTIN_DJANGO_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
     assert config['LOCATION'] == 'redis://:redispass@127.0.0.1:6379/0'
     assert 'PASSWORD' not in config.get('OPTIONS', {})
     assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
     assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
 
 
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_hiredis_with_username_dj3():
-    importlib.reload(django_cache_url)
+def test_hiredis_with_username_dj3(django_cache_url_dj3):
     url = 'hiredis://foo:@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj3.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DEFAULT_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj3.DEFAULT_REDIS_BACKEND
     assert config['LOCATION'] == 'redis://foo@127.0.0.1:6379/0'
     assert 'PASSWORD' not in config.get('OPTIONS', {})
     assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
 
 
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_hiredis_with_username_dj4():
-    importlib.reload(django_cache_url)
+def test_hiredis_with_username_dj4(django_cache_url_dj4):
     url = 'hiredis://foo:@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj4.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.BUILTIN_DJANGO_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
     assert config['LOCATION'] == 'redis://foo:@127.0.0.1:6379/0'
     assert 'PASSWORD' not in config.get('OPTIONS', {})
     assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
     assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
 
 
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_hiredis_with_username_password_dj3():
-    importlib.reload(django_cache_url)
+def test_hiredis_with_username_password_dj3(django_cache_url_dj3):
     url = 'hiredis://foo:bar@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj3.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DEFAULT_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj3.DEFAULT_REDIS_BACKEND
     assert config['LOCATION'] == 'redis://foo@127.0.0.1:6379/0'
     assert config['OPTIONS']['PASSWORD'] == 'bar'
     assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
 
 
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_hiredis_with_username_password_dj4():
-    importlib.reload(django_cache_url)
+def test_hiredis_with_username_password_dj4(django_cache_url_dj4):
     url = 'hiredis://foo:bar@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj4.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.BUILTIN_DJANGO_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
     assert config['LOCATION'] == 'redis://foo:bar@127.0.0.1:6379/0'
     assert 'PASSWORD' not in config.get('OPTIONS', {})
     assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
     assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
+# endregion
 
 
-#
-# REDIS
-#
-
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_redis_dj3():
-    importlib.reload(django_cache_url)
+# region REDIS url
+def test_redis_dj3(django_cache_url_dj3):
     url = 'redis://127.0.0.1:6379/0?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj3.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DJANGO_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj3.DJANGO_REDIS_BACKEND
     assert config['LOCATION'] == 'redis://127.0.0.1:6379/0'
     assert config['KEY_PREFIX'] == 'site1'
 
 
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_redis_dj4():
-    importlib.reload(django_cache_url)
+def test_redis_dj4(django_cache_url_dj4):
     url = 'redis://127.0.0.1:6379/0?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj4.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.BUILTIN_DJANGO_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
     assert config['LOCATION'] == 'redis://127.0.0.1:6379/0'
     assert config['KEY_PREFIX'] == 'site1'
 
 
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_redis_socket_dj3():
-    importlib.reload(django_cache_url)
+def test_redis_with_password_dj3(django_cache_url_dj3):
+    url = 'redis://:redispass@127.0.0.1:6379/0'
+    config = django_cache_url_dj3.parse(url)
+
+    assert config['BACKEND'] == django_cache_url_dj3.DEFAULT_REDIS_BACKEND
+    assert config['LOCATION'] == 'redis://127.0.0.1:6379/0'
+    assert config['OPTIONS']['PASSWORD'] == 'redispass'
+
+
+def test_redis_with_password_dj4(django_cache_url_dj4):
+    url = 'redis://:redispass@127.0.0.1:6379/0'
+    config = django_cache_url_dj4.parse(url)
+
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
+    assert config['LOCATION'] == 'redis://:redispass@127.0.0.1:6379/0'
+    assert 'PASSWORD' not in config.get('OPTIONS', {})
+
+
+def test_redis_with_username_dj3(django_cache_url_dj3):
+    url = 'redis://foo:@127.0.0.1:6379/0'
+    config = django_cache_url_dj3.parse(url)
+
+    assert config['BACKEND'] == django_cache_url_dj3.DEFAULT_REDIS_BACKEND
+    assert config['LOCATION'] == 'redis://foo@127.0.0.1:6379/0'
+    assert 'PASSWORD' not in config.get('OPTIONS', {})
+
+
+def test_redis_with_username_dj4(django_cache_url_dj4):
+    url = 'redis://foo:@127.0.0.1:6379/0'
+    config = django_cache_url_dj4.parse(url)
+
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
+    assert config['LOCATION'] == 'redis://foo:@127.0.0.1:6379/0'
+    assert 'PASSWORD' not in config.get('OPTIONS', {})
+
+
+def test_redis_with_username_password_dj3(django_cache_url_dj3):
+    url = 'redis://foo:bar@127.0.0.1:6379/0'
+    config = django_cache_url_dj3.parse(url)
+
+    assert config['BACKEND'] == django_cache_url_dj3.DEFAULT_REDIS_BACKEND
+    assert config['LOCATION'] == 'redis://foo@127.0.0.1:6379/0'
+    assert config['OPTIONS']['PASSWORD'] == 'bar'
+
+
+def test_redis_with_username_password_dj4(django_cache_url_dj4):
+    url = 'redis://foo:bar@127.0.0.1:6379/0'
+    config = django_cache_url_dj4.parse(url)
+
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
+    assert config['LOCATION'] == 'redis://foo:bar@127.0.0.1:6379/0'
+    assert 'PASSWORD' not in config.get('OPTIONS', {})
+# endregion
+
+
+# region REDIS socket
+def test_redis_socket_dj3(django_cache_url_dj3):
     url = 'redis:///path/to/socket/1?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj3.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DJANGO_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj3.DJANGO_REDIS_BACKEND
     assert config['LOCATION'] == 'unix:///path/to/socket?db=1'
     assert 'OPTIONS' not in config
     assert config['KEY_PREFIX'] == 'site1'
 
 
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_redis_socket_dj4():
-    importlib.reload(django_cache_url)
+def test_redis_socket_dj4(django_cache_url_dj4):
     url = 'redis:///path/to/socket/1?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj4.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.BUILTIN_DJANGO_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
     assert config['LOCATION'] == 'unix:///path/to/socket?db=1'
     assert 'OPTIONS' not in config
     assert config['KEY_PREFIX'] == 'site1'
 
 
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_redis_socket_with_username_password_dj3():
-    importlib.reload(django_cache_url)
+def test_redis_socket_with_username_password_dj3(django_cache_url_dj3):
     url = 'redis://foo:bar@/path/to/socket/1?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj3.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DJANGO_REDIS_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj3.DJANGO_REDIS_BACKEND
     assert config['LOCATION'] == 'unix://foo:bar@/path/to/socket?db=1'
     assert 'OPTIONS' not in config
     assert config['KEY_PREFIX'] == 'site1'
 
 
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_redis_socket_with_username_password_dj4():
-    importlib.reload(django_cache_url)
+def test_redis_socket_with_username_password_dj4(django_cache_url_dj4):
     url = 'redis://foo:bar@/path/to/socket/1?key_prefix=site1'
-    config = django_cache_url.parse(url)
+    config = django_cache_url_dj4.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.BUILTIN_DJANGO_BACKEND
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
     assert config['LOCATION'] == 'unix://foo@/path/to/socket?db=1&password=bar'
     assert 'OPTIONS' not in config
     assert config['KEY_PREFIX'] == 'site1'
+# endregion
 
 
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_redis_with_password_dj3():
-    importlib.reload(django_cache_url)
-    url = 'redis://:redispass@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
+# region REDISS
+def test_rediss_dj3(django_cache_url_dj3):
+    url = 'rediss://127.0.0.1:6379'
+    config = django_cache_url_dj3.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.DEFAULT_REDIS_BACKEND
-    assert config['LOCATION'] == 'redis://127.0.0.1:6379/0'
-    assert config['OPTIONS']['PASSWORD'] == 'redispass'
+    assert config['BACKEND'] == django_cache_url_dj3.DJANGO_REDIS_BACKEND
+    assert config['LOCATION'] == 'rediss://127.0.0.1:6379/0'
 
 
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_redis_with_password_dj4():
-    importlib.reload(django_cache_url)
-    url = 'redis://:redispass@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
+def test_rediss_dj4(django_cache_url_dj4):
+    url = 'rediss://127.0.0.1:6379'
+    config = django_cache_url_dj4.parse(url)
 
-    assert config['BACKEND'] == django_cache_url.BUILTIN_DJANGO_BACKEND
-    assert config['LOCATION'] == 'redis://:redispass@127.0.0.1:6379/0'
-    assert 'PASSWORD' not in config.get('OPTIONS', {})
-
-
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_redis_with_username_dj3():
-    importlib.reload(django_cache_url)
-    url = 'redis://foo:@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
-
-    assert config['BACKEND'] == django_cache_url.DEFAULT_REDIS_BACKEND
-    assert config['LOCATION'] == 'redis://foo@127.0.0.1:6379/0'
-    assert 'PASSWORD' not in config.get('OPTIONS', {})
-
-
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_redis_with_username_dj4():
-    importlib.reload(django_cache_url)
-    url = 'redis://foo:@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
-
-    assert config['BACKEND'] == django_cache_url.BUILTIN_DJANGO_BACKEND
-    assert config['LOCATION'] == 'redis://foo:@127.0.0.1:6379/0'
-    assert 'PASSWORD' not in config.get('OPTIONS', {})
-
-
-@mock.patch('django.VERSION', (3, 0, 0, "final", 0))
-def test_redis_with_username_password_dj3():
-    importlib.reload(django_cache_url)
-    url = 'redis://foo:bar@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
-
-    assert config['BACKEND'] == django_cache_url.DEFAULT_REDIS_BACKEND
-    assert config['LOCATION'] == 'redis://foo@127.0.0.1:6379/0'
-    assert config['OPTIONS']['PASSWORD'] == 'bar'
-
-
-@mock.patch('django.VERSION', (4, 0, 0, "final", 0))
-def test_redis_with_username_password_dj4():
-    importlib.reload(django_cache_url)
-    url = 'redis://foo:bar@127.0.0.1:6379/0'
-    config = django_cache_url.parse(url)
-
-    assert config['BACKEND'] == django_cache_url.BUILTIN_DJANGO_BACKEND
-    assert config['LOCATION'] == 'redis://foo:bar@127.0.0.1:6379/0'
-    assert 'PASSWORD' not in config.get('OPTIONS', {})
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
+    assert config['LOCATION'] == 'rediss://127.0.0.1:6379/0'
+# endregion
