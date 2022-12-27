8682074203032f43f965fb304b904de7c3dd2e6a
Nagico <yjr888@vip.qq.com>
2022-12-27 12:35:30 +0800

    Fix hiredis url-base test region

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -18,50 +18,6 @@ def test_hiredis_dj4(django_cache_url_dj4):
     assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
     assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
     assert config['OPTIONS']['pool_class'] == 'redis.BlockingConnectionPool'
-# endregion
-
-
-# region HIREDIS socket
-def test_hiredis_socket_dj3(django_cache_url_dj3):
-    url = 'hiredis:///path/to/socket/1?key_prefix=site1'
-    config = django_cache_url_dj3.parse(url)
-
-    assert config['BACKEND'] == django_cache_url_dj3.DEFAULT_REDIS_BACKEND
-    assert config['LOCATION'] == 'unix:///path/to/socket?db=1'
-    assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
-
-
-def test_hiredis_socket_dj4(django_cache_url_dj4):
-    url = 'hiredis:///path/to/socket/1?key_prefix=site1'
-    config = django_cache_url_dj4.parse(url)
-
-    assert config['BACKEND'] == django_cache_url_dj4.DEFAULT_REDIS_BACKEND
-    assert config['LOCATION'] == 'unix:///path/to/socket?db=1'
-    assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
-    assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
-
-
-def test_hiredis_socket_with_username_password_dj3(django_cache_url_dj3):
-    url = 'hiredis://foo:bar@/path/to/socket/1?key_prefix=site1'
-    config = django_cache_url_dj3.parse(url)
-
-    assert config['BACKEND'] == django_cache_url_dj3.DJANGO_REDIS_BACKEND
-    assert config['LOCATION'] == 'unix://foo:bar@/path/to/socket?db=1'
-    assert 'PASSWORD' not in config.get('OPTIONS', {})
-    assert config['KEY_PREFIX'] == 'site1'
-    assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
-
-
-def test_hiredis_socket_with_username_password_dj4(django_cache_url_dj4):
-    url = 'hiredis://foo:bar@/path/to/socket/1?key_prefix=site1'
-    config = django_cache_url_dj4.parse(url)
-
-    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
-    assert config['LOCATION'] == 'unix://foo@/path/to/socket?db=1&password=bar'
-    assert 'PASSWORD' not in config.get('OPTIONS', {})
-    assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
-    assert config['KEY_PREFIX'] == 'site1'
-    assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
 
 
 def test_hiredis_with_password_dj3(django_cache_url_dj3):
@@ -124,6 +80,50 @@ def test_hiredis_with_username_password_dj4(django_cache_url_dj4):
     assert config['LOCATION'] == 'redis://foo:bar@127.0.0.1:6379/0'
     assert 'PASSWORD' not in config.get('OPTIONS', {})
     assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
+    assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
+# endregion
+
+
+# region HIREDIS socket
+def test_hiredis_socket_dj3(django_cache_url_dj3):
+    url = 'hiredis:///path/to/socket/1?key_prefix=site1'
+    config = django_cache_url_dj3.parse(url)
+
+    assert config['BACKEND'] == django_cache_url_dj3.DEFAULT_REDIS_BACKEND
+    assert config['LOCATION'] == 'unix:///path/to/socket?db=1'
+    assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
+
+
+def test_hiredis_socket_dj4(django_cache_url_dj4):
+    url = 'hiredis:///path/to/socket/1?key_prefix=site1'
+    config = django_cache_url_dj4.parse(url)
+
+    assert config['BACKEND'] == django_cache_url_dj4.DEFAULT_REDIS_BACKEND
+    assert config['LOCATION'] == 'unix:///path/to/socket?db=1'
+    assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
+    assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
+
+
+def test_hiredis_socket_with_username_password_dj3(django_cache_url_dj3):
+    url = 'hiredis://foo:bar@/path/to/socket/1?key_prefix=site1'
+    config = django_cache_url_dj3.parse(url)
+
+    assert config['BACKEND'] == django_cache_url_dj3.DJANGO_REDIS_BACKEND
+    assert config['LOCATION'] == 'unix://foo:bar@/path/to/socket?db=1'
+    assert 'PASSWORD' not in config.get('OPTIONS', {})
+    assert config['KEY_PREFIX'] == 'site1'
+    assert config['OPTIONS']['PARSER_CLASS'] == 'redis.connection.HiredisParser'
+
+
+def test_hiredis_socket_with_username_password_dj4(django_cache_url_dj4):
+    url = 'hiredis://foo:bar@/path/to/socket/1?key_prefix=site1'
+    config = django_cache_url_dj4.parse(url)
+
+    assert config['BACKEND'] == django_cache_url_dj4.BUILTIN_DJANGO_BACKEND
+    assert config['LOCATION'] == 'unix://foo@/path/to/socket?db=1&password=bar'
+    assert 'PASSWORD' not in config.get('OPTIONS', {})
+    assert 'PARSER_CLASS' not in config.get('OPTIONS', {})
+    assert config['KEY_PREFIX'] == 'site1'
     assert config['OPTIONS']['parser_class'] == 'redis.connection.HiredisParser'
 # endregion
 
